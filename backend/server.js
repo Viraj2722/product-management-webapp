d54e8bc32baedcb991483e4b63f67e66c4aef9ae
Viraj Walavalkar <virajwalavalkar90982@gmail.com>
2024-09-17 23:09:21 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -4,30 +4,25 @@ const path = require("path");
 const connectDB = require("./config/db");
 const Product = require("./models/product.model");
 
-// Load environment variables
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware to parse JSON
 app.use(express.json());
 
-// Serve static files in production
 if (process.env.NODE_ENV === "production") {
     const frontendPath = path.join(__dirname, "../frontend/dist");
     console.log("Serving static files from:", frontendPath);
     app.use(express.static(frontendPath));
 }
 
-// Serve the index.html file for all other routes
 app.get("*", (req, res) => {
     const indexPath = path.resolve(__dirname, "../frontend/dist", "index.html");
     console.log("Serving file:", indexPath);
     res.sendFile(indexPath);
 });
 
-// API routes
 app.get("/api/products", async (req, res) => {
     try {
         const products = await Product.find({});
@@ -75,7 +70,6 @@ app.delete("/api/products/:id", async (req, res) => {
     }
 });
 
-// Connect to DB and start server
 connectDB();
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
